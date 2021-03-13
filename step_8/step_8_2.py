d92eed3144c7cb4e1522daded046916a10cc5785
NS-3 <78708166+NS-3@users.noreply.github.com>
2021-03-13 19:48:35 +0300

    дз 7 урок

---
+++ step_8/step_8_2.py
@@ -0,0 +1,17 @@
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+inp_data1 = input("Введите делимое: ")
+inp_data2 = input("Введите делитель: ")
+try:
+    inp_data1 = int(inp_data1)
+    inp_data2 = int(inp_data2)
+    if inp_data2 == 0:
+        raise OwnError("Делить на 0 нельзя")
+except ValueError:
+    print("Вы ввели не число")
+except OwnError as err:
+    print(err)
+else:
+    print(f"Все хорошо. Ваш результат: {inp_data1/inp_data2}")
